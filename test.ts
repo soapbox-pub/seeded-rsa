8a381fafc30de4aefa9da48cf5992028036fd4b2
Alex Gleason <alex@alexgleason.me>
2023-02-14 19:01:25 -0600

    Make tests DRY

--- test.ts
+++ test.ts
@@ -5,15 +5,16 @@ import { generateSeededRsa } from './mod.ts';
 Deno.test('generateSeededRsa', async () => {
   const keys = await generateSeededRsa('alex@gleasonator.com:benis911');
   const message = new TextEncoder().encode('hello world!');
+  const algorithm = 'RSASSA-PKCS1-v1_5';
 
   const signature = await window.crypto.subtle.sign(
-    'RSASSA-PKCS1-v1_5',
+    algorithm,
     keys.privateKey,
     message,
   );
 
   const valid = await crypto.subtle.verify(
-    'RSASSA-PKCS1-v1_5',
+    algorithm,
     keys.publicKey,
     signature,
     message,
